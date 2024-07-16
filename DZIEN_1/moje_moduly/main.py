1cdd0e2164074ae4d6301557066b483ed2b60194
albim72 <marcin.albiniak72@gmail.com>
2024-07-16 11:58:42 +0200

    Update main.py

--- DZIEN_1/moje_moduly/main.py
+++ DZIEN_1/moje_moduly/main.py
@@ -2,6 +2,7 @@
 # import dane as dn
 from dane import nrfilii as nf
 from dane import book as bk
+from funkcje.kolekcje import czytaj_liste,czytaj_slownik
 
 
 print("użycie modułów poprzez import...")
@@ -10,3 +11,9 @@ print("użycie modułów poprzez import...")
 
 print(nf)
 print(bk)
+
+print("użycie funkcji: czytaj_liste():")
+czytaj_liste(nf)
+
+print("użycie funkcji: czytaj_slownik():")
+czytaj_slownik(bk)
