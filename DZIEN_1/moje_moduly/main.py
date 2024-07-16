73ea9d2167d90b4df77a00d40754c602eb7affa0
albim72 <marcin.albiniak72@gmail.com>
2024-07-16 11:42:24 +0200

    Create main.py

---
+++ DZIEN_1/moje_moduly/main.py
@@ -0,0 +1,12 @@
+# import dane
+# import dane as dn
+from dane import nrfilii as nf
+from dane import book as bk
+
+
+print("użycie modułów poprzez import...")
+# CTRL+D - powielanie linii/bloku kodu
+# CTRL + / komentowanie/odkomentowanie bloku kodu
+
+print(nf)
+print(bk)
