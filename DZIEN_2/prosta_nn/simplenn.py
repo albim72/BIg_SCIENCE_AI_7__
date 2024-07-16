1b46cb265fc1d394fe9a51f3fbb786495fa3232e
albim72 <marcin.albiniak72@gmail.com>
2024-07-16 16:02:25 +0200

    Create simplenn.py

---
+++ DZIEN_2/prosta_nn/simplenn.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class SimpleNeuralNetwork:
+    def __init__(self):
+        pass
+
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(SimpleNeuralNetwork)
