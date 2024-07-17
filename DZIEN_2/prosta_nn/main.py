e8dfe59a26141b546d8c3d0132bad3ca404f8ec4
albim72 <marcin.albiniak72@gmail.com>
2024-07-17 10:07:12 +0200

    Update main.py

--- DZIEN_2/prosta_nn/main.py
+++ DZIEN_2/prosta_nn/main.py
@@ -1,31 +1,20 @@
 import numpy as np
+from simplenn import SimpleNeuralNetwork
 
-class SimpleNeuralNetwork:
-    def __init__(self):
-        np.random.seed(1)
-        self.weights = 2*np.random.random((3,1))-1
+network = SimpleNeuralNetwork()
+print(network)
+# print(network.weights)
 
-    def __repr__(self):
-        return (f"Nowa siec neuronowa oparta na klasie: {self.__class__.__name__}\n"
-                f"Wylosowano wagi z ziarnem = 1:\n{self.weights}")
+train_inputs = np.array([[1,1,0],[1,1,1],[1,1,0],[1,0,0],[0,1,1],[0,1,0],])
+train_output = np.array([[0,1,0,0,1,0]]).T
+train_iterators = 50_000
 
-    # def __new__(cls, *args, **kwargs):
-    #     return object.__new__(SimpleNeuralNetwork)
-    
-    def sigmoid(self,x):
-        return 1/(1+np.exp(-x))
-    
-    def d_sigmoid(self,x):
-        return x*(1-x)
-    
-    def propagation(self,inputs):
-        return self.sigmoid(np.dot(inputs.astype(float),self.weights))
-    
-    def backward_propagation(self,propagation_result,train_input,train_output):
-        error = train_output - propagation_result
-        self.weights += np.dot(train_input.T,error*self.d_sigmoid(propagation_result))
-        
-    def train(self,train_input,train_output,train_iters):
-        for _ in range(train_iters):
-            propagation_result = self.propagation(train_input)
-            self.backward_propagation(propagation_result,train_input,train_output)
+
+network.train(train_inputs,train_output,train_iterators)
+print(network.weights)
+
+print("ocena modelu")
+testdata = np.array([[1,1,1],[1,0,0],[0,1,1],[0,1,0],[0,0,1],[0,0,0],])
+
+for data in testdata:
+    print(f"wynik dla {data} wynosi: {network.propagation(data)}")
