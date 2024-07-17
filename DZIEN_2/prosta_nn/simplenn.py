90e3d9050ae70604bfb347e7cc2dc04a9683fffc
albim72 <marcin.albiniak72@gmail.com>
2024-07-17 09:43:32 +0200

    Update simplenn.py

--- DZIEN_2/prosta_nn/simplenn.py
+++ DZIEN_2/prosta_nn/simplenn.py
@@ -2,7 +2,12 @@ import numpy as np
 
 class SimpleNeuralNetwork:
     def __init__(self):
-        pass
+        np.random.seed(1)
+        self.weights = 2*np.random.random((3,1))-1
+
+    def __repr__(self):
+        return (f"Nowa siec neuronowa oparta na klasie: {self.__class__.__name__}\n"
+                f"Wylosowano wagi z ziarnem = 1:\n{self.weights}")
 
     # def __new__(cls, *args, **kwargs):
     #     return object.__new__(SimpleNeuralNetwork)
