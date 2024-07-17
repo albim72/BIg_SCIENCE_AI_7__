14ec5943a0fe6f9c10817d6c14d76bb5203cf8f6
albim72 <marcin.albiniak72@gmail.com>
2024-07-17 09:53:56 +0200

    Update main.py

--- DZIEN_2/prosta_nn/main.py
+++ DZIEN_2/prosta_nn/main.py
@@ -1,5 +1,31 @@
 import numpy as np
-from simplenn import SimpleNeuralNetwork
 
-network = SimpleNeuralNetwork()
-print(network)
+class SimpleNeuralNetwork:
+    def __init__(self):
+        np.random.seed(1)
+        self.weights = 2*np.random.random((3,1))-1
+
+    def __repr__(self):
+        return (f"Nowa siec neuronowa oparta na klasie: {self.__class__.__name__}\n"
+                f"Wylosowano wagi z ziarnem = 1:\n{self.weights}")
+
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(SimpleNeuralNetwork)
+    
+    def sigmoid(self,x):
+        return 1/(1+np.exp(-x))
+    
+    def d_sigmoid(self,x):
+        return x*(1-x)
+    
+    def propagation(self,inputs):
+        return self.sigmoid(np.dot(inputs.astype(float),self.weights))
+    
+    def backward_propagation(self,propagation_result,train_input,train_output):
+        error = train_output - propagation_result
+        self.weights += np.dot(train_input.T,error*self.d_sigmoid(propagation_result))
+        
+    def train(self,train_input,train_output,train_iters):
+        for _ in range(train_iters):
+            propagation_result = self.propagation(train_input)
+            self.backward_propagation(propagation_result,train_input,train_output)
